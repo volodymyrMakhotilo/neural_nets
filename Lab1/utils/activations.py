98336798379babd99e2f48971436ee9556e32e31
Androklaw <andrii.pestun.gamedev@gmail.com>
2023-10-07 20:04:53 +0300

    added all activation and cost functions

--- Lab1/utils/activations.py
+++ Lab1/utils/activations.py
@@ -1,7 +1,7 @@
 from abc import ABC, abstractmethod
 
 import numpy as np
-
+import math as m
 
 class Activation_Function(ABC):
     @abstractmethod
@@ -18,20 +18,35 @@ class ReLU(Activation_Function):
         return np.maximum(0, x)
 
     def derivative(self, x):
-        dx = x * (x >= 0).astype(np.uint)
+        dx = (x >= 0).astype(np.uint)
         return dx
 
 class Sigmoid(Activation_Function):
-    pass
+    def compute(self, x):
+        return 1 / (1 + np.exp(-x))
+
+    def derivative(self, x):
+        return self.compute(x) * (1 - self.compute(x))
 
 class TanH(Activation_Function):
-    pass
+    def compute(self, x):
+        return m.tanh(x)
+
+    def derivative(self, x):
+        return 1 - self.compute(x) ** 2
 
 class LeakyReLU(Activation_Function):
-    pass
+    def compute(self, x):
+        return np.maximum(0.01 * x, x)
+    def derivative(self, x):
+        if x < 0: return 0.01
+        else: return 1
 
 class Linear(Activation_Function):
-    pass
+    def compute(self, x):
+        return x
+    def derivative(self, x):
+        return 1
 
 class Softmax(Activation_Function):
     def compute(self, x):
--- Lab1/utils/cost_functions.py
+++ Lab1/utils/cost_functions.py
@@ -12,7 +12,15 @@ class Cost_Function(ABC):
 
 
 class Binary_Crossentropy(Cost_Function):
-    pass
+    def compute(self, y_true, y_pred):
+        epsilon = 1e-15 # Small constant to avoid division by zero
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon) # Clip values to avoid numerical instability
+        return -np.mean(y_true * np.log(y_pred) + (1 - y_true) * np.log(1 - y_pred))
+
+    def derivative(self, y_true, y_pred):
+        epsilon = 1e-15
+        y_pred = np.clip(y_pred, epsilon, 1 - epsilon)
+        return (y_pred - y_true) / (y_pred * (1 - y_pred))
 
 # Log N is under question
 # y_true should be sparse
@@ -27,7 +35,21 @@ class Sparse_Categorical_Crossentropy(Cost_Function):
 
 
 class MAE(Cost_Function):
-    pass
+    def compute(self, y_true, y_pred):
+        return np.mean(np.abs(y_true - y_pred))
+
+    def derivative(self, y_true, y_pred):
+        if y_pred < y_true:
+            return -1.0
+        elif y_pred > y_true:
+            return 1.0
+        else:
+            # Return any value between -1 and 1 for the subderivative at zero
+            return 0.0
 
 class MSE(Cost_Function):
-    pass
+    def compute(self, y_true, y_pred):
+        return np.mean((y_true - y_pred) ** 2)
+
+    def derivative(self, y_true, y_pred):
+        return 2 * (y_pred - y_true) / len(y_true)
