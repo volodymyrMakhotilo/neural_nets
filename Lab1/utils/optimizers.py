1c8653080f334ebf92e93162a23b6f633c5e4f4b
volodymyrMakhotilo <vkmahotilo@gmail.com>
2023-11-02 21:26:40 +0200

    added grad clip for much more stable learning

--- Lab1/utils/optimizers.py
+++ Lab1/utils/optimizers.py
@@ -1,7 +1,7 @@
 # THIS FILE IS A MESS
-import  numpy as np
+import numpy as np
 
 alpha = 6
 
 def gradient_descent(x, dx, epoch):
-    return x - 0.99 ** np.sqrt(epoch) * alpha * dx
+    return x - np.clip(0.99 ** epoch * alpha * dx, -1, 1)
